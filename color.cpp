d91928945ee85838584d57fb86486648da4b283a
Hubert Jasieniecki <hubert@jasieniecki.com>
2019-04-19 14:54:31 +0200

    Implement "addtion" blending mode

--- color.cpp
+++ color.cpp
@@ -21,6 +21,10 @@ Color Color::operator+(Color other)
     ret.blue = blue + other.blue;
     ret.green = green + other.green;
     ret.alpha = alpha + other.alpha;
+    if (ret.red > 0xff) ret.red = 0xff;
+    if (ret.blue > 0xff) ret.blue = 0xff;
+    if (ret.green > 0xff) ret.green = 0xff;
+    if (ret.alpha > 0xff) ret.alpha = 0xff;
     return ret;
 }
 
