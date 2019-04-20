56c9d00bd436b628c04fd7160bbe5e4a783a2bf7
Hubert Jasieniecki <hubert@jasieniecki.com>
2019-04-20 09:15:21 +0200

    Move alpha sliders to 1.0 by default

--- image.cpp
+++ image.cpp
@@ -13,7 +13,7 @@ Image::Image(std::__cxx11::string path)
     :
       visible_(false),
       mode(BlendingMode::Normal),
-      alpha_(0.0),
+      alpha_(1.0),
       qImage(path.c_str()),
       bits(qImage.bits())
 {}
