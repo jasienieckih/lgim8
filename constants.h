bd7a081ac3185dc738b68009896a5d05d6ef73d7
Hubert Jasieniecki <hubert@jasieniecki.com>
2019-07-30 12:01:24 +0200

    Add points and triangles for whole pictures

--- constants.h
+++ constants.h
@@ -1,8 +1,8 @@
 #ifndef CONSTANTS
 #define CONSTANTS
 
-const int NUMBER_OF_TRIANGLES = 12;
-const int NUMBER_OF_POINTS = 12;
+const int NUMBER_OF_TRIANGLES = 104;
+const int NUMBER_OF_POINTS = 60;
 
 #endif // CONSTANTS
 
